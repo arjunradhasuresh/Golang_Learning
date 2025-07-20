c717811945ec79757f3bbb29d7ce0aaad5c884f9
arjunradhasuresh <arjunradhasuresh@gmail.com>
2025-07-20 06:47:23 +0530

    Add mutex lock

---
+++ mutex/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	fmt.Println("ener the number")
+	var wg sync.WaitGroup
+	var lk sync.Mutex
+	counter := 0
+
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go increment(&wg, &lk, &counter)
+	}
+	wg.Wait()
+	fmt.Println(counter)
+}
+
+func increment(wg *sync.WaitGroup, lk *sync.Mutex, counter *int) {
+	defer wg.Done()
+	lk.Lock()
+	*counter++
+	lk.Unlock()
+}
